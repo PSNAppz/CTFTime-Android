88d4dffdb8895124bc737998cc973a0ee455af90
PS Narayanan <thepsnarayanan@gmail.com>
2018-04-04 19:04:18 +0530

    Events Module Final

--- app/src/main/java/com/neonsec/ctftime/ctf_timemobile/MainActivity.java
+++ app/src/main/java/com/neonsec/ctftime/ctf_timemobile/MainActivity.java
@@ -1,6 +1,7 @@
 package com.neonsec.ctftime.ctf_timemobile;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,10 +9,37 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private AdView mAdView;
+    private AdRequest adRequest;
+    private CardsAdapter adapter;
+    private CardModel ctf;
+    private TextView title,type,url,onsite,start;
+    private ImageView eventImage;
+    String API_URL = "";
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +48,16 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ListView lvCards = (ListView) findViewById(R.id.list_cards);
+        adapter = new CardsAdapter(this);
+
+        lvCards.setAdapter(adapter);
+        title = findViewById(R.id.title);
+        type = findViewById(R.id.type);
+        url = findViewById(R.id.url);
+        onsite = findViewById(R.id.onsite);
+        start = findViewById(R.id.start);
+        eventImage = findViewById(R.id.image);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -29,6 +67,146 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3041411382010702/9282911856");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the interstitial ad is closed.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                mAdView.loadAd(adRequest);
+
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                mAdView.loadAd(adRequest);
+
+            }
+        });
+        new MainActivity.RetrieveFeedTask().execute();
+
+    }
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+
+        }
+
+        protected String doInBackground(Void... urls) {
+            // Do some validation here
+            long unixTime = System.currentTimeMillis() / 1000L;
+            long unixTimeFuture = System.currentTimeMillis()+865000000 / 1000L;
+            API_URL = "https://ctftime.org/api/v1/events/?limit=100&start="+unixTime+"&finish="+unixTimeFuture;
+            Log.d("API URL: ",API_URL);
+
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            //Log.i("INFO", response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+            try {
+                for(int i=0;i<response.length() -1 ;i++){
+                    int imageID = R.drawable.ic_launcher_background;
+                    String title,url,start,type,imageURL;
+                    Boolean onsite;
+
+                    //JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                    String events = response;
+                    JSONArray eventArray = new JSONArray(events);
+                    JSONObject current = (JSONObject) new JSONTokener(eventArray.getString(i)).nextValue();
+                    Log.i("EVENT",current.getString("title"));
+                    title = current.getString("title");
+                    url = current.getString("url");
+                    start = current.getString("start");
+                    type = current.getString("format");
+                    onsite = current.getBoolean("onsite");
+                    imageURL = current.getString("logo");
+                    ctf = new CardModel(imageID,title, type,url, start, onsite,imageURL);
+                    adapter.addAll(ctf);
+                    adapter.notifyDataSetChanged();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -64,19 +242,36 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             //Events
+            if (mInterstitialAd.isLoaded()) {
+                mInterstitialAd.show();
+            } else {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
             Intent Events = new Intent(MainActivity.this,MainActivity.class);
             startActivity(Events);
 
         } else if (id == R.id.nav_gallery) {
             //Top 10 Teams
+            if (mInterstitialAd.isLoaded()) {
+                mInterstitialAd.show();
+            } else {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
             Intent TopTen = new Intent(MainActivity.this,TopTenTeams.class);
             startActivity(TopTen);
         } else if (id == R.id.nav_share) {
             //Share appstore link
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = "https://play.google.com/store/apps/details?id=com.neonsec.ctftime.ctf_timemobile";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "CTF Time Unofficial App");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
 
         } else if (id == R.id.nav_send) {
             //About the app
-
+            Intent About = new Intent(MainActivity.this,AboutActivity.class);
+            startActivity(About);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
